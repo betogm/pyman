7c68dfe421ed30aec6a11e3f092a63206aea8a25
Huberto Gastal Mayer <huberto.mayer@executiva.com.br>
2025-10-28 10:42:45 -0300

    fix: environment_var

--- example_collection/post-request/post-data-pre-script.py
+++ example_collection/post-request/post-data-pre-script.py
@@ -3,10 +3,10 @@
 print("[REQ PRE Script]: Preparing to send the POST.")
 
 # Checks if the variable from the previous request (GET) exists
-if 'LAST_ORIGIN_IP' in env:
-    print(f"[REQ PRE Script]: The previous request (GET) saved the IP: {env['LAST_ORIGIN_IP']}")
+if 'LAST_ORIGIN_IP' in environment_vars:
+    print(f"[REQ PRE Script]: The previous request (GET) saved the IP: {environment_vars['LAST_ORIGIN_IP']}")
 else:
     print("[REQ PRE Script]: 'LAST_ORIGIN_IP' variable not yet defined (run the GET first).")
     # Defines a default value if it doesn't exist
-    if 'LAST_origin_ip' not in env:
-        env['LAST_ORIGIN_IP'] = "not-defined"
+    if 'LAST_origin_ip' not in environment_vars:
+        environment_vars['LAST_ORIGIN_IP'] = "not-defined"
